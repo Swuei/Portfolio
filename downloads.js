c1b1890828189ac334be30ab7bc4e11b485bdd58
Swuei <Swueistaff@gmail.com>
2025-07-24 15:10:51 -0400

    Update downloads.js

--- downloads.js
+++ downloads.js
@@ -1,4 +1,4 @@
-﻿document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function () {
     if (typeof AOS !== 'undefined') {
         AOS.init({
             duration: 800,
@@ -328,28 +328,34 @@
             shareButton.addEventListener('click', handleSharePageClick);
         }
     }
-        document.addEventListener('contextmenu', function(event) {
-            event.preventDefault();
+
+    document.addEventListener('contextmenu', function (event) {
+        event.preventDefault();
     });
 
-        document.onkeydown = function(event) {
-        if (event.key === 'F12' ||
-        (event.ctrlKey && event.shiftKey && (event.key === 'I' || event.key === 'C' || event.key === 'J')) ||
-        (event.ctrlKey && event.key === 'U') ||
-        (event.ctrlKey && event.key === 'S') ||
-        (event.ctrlKey && event.shiftKey && event.key === 'E')) {
+    document.onkeydown = function (event) {
+        if (
+            event.key === 'F12' ||
+            (event.ctrlKey && event.shiftKey && (event.key === 'I' || event.key === 'C' || event.key === 'J')) ||
+            (event.ctrlKey && event.key === 'U') ||
+            (event.ctrlKey && event.key === 'S') ||
+            (event.ctrlKey && event.shiftKey && event.key === 'E')
+        ) {
             event.preventDefault();
-        return false;
+            return false;
         }
     };
 
-        document.onselectstart = function(event) {
-            event.preventDefault();
+    document.onselectstart = function (event) {
+        event.preventDefault();
         return false;
     };
 
     setInterval(function () {
-        if (window.outerWidth - window.innerWidth > 100 || window.outerHeight - window.innerHeight > 100) {
+        const start = performance.now();
+        debugger;
+        const timeTaken = performance.now() - start;
+        if (timeTaken > 100) {
             alert('Please close DevTools to continue.');
             location.reload();
         }
